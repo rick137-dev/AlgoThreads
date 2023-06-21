0fdfdc8fd81eadc6d484ffb09bddc382fb65e352
rick137-dev <caiulorick137@gmail.com>
2023-06-21 13:04:31 +0200

    Added structure to program, matrix, read from user and print statements

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,91 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public int[][] Multiply(int[][] matrix1, int[][] matrix2){
+
+
+        return null;
     }
 
-    //Idea per algo: moltiplicazione di 2 matrici NxN con i thread
-}
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to our program!\n");
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size of arrays N\n");
+
+        Integer N = Integer.parseInt(scanner.nextLine());
+        int n = N.intValue();
+        int i,j;
+        int[][] matrix1 = new int[n][n];
+        int[][] matrix2 = new int[n][n];
+        System.out.println("Enter values of array 1\n");
+        Integer temp;
+
+
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++) {
+                temp = Integer.parseInt(scanner.nextLine());
+                matrix1[i][j] = temp.intValue();
+            }
+            }
+
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++) {
+               System.out.print(matrix1[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+
+        System.out.println("Enter values of array 2\n");
+
+
+
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++) {
+                temp = Integer.parseInt(scanner.nextLine());
+                matrix2[i][j] = temp.intValue();
+            }
+        }
+
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++) {
+                System.out.print(matrix2[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+
+        int[][] finalMat = new int[n][n];
+
+
+        System.out.print("\n");
+        System.out.print("The multiplication of the first Matrix with the Second is:\n");
+
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++) {
+                System.out.print(finalMat[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
